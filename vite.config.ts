777d9f1bd46cd72b43c2b12ce306000971f23622
gyiyang <gyiyang@trip.com>
2026-05-10 10:18:23 +0800

    chore: deploy in github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
   root: '.',
+  base: '/DIY-dungeon/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
